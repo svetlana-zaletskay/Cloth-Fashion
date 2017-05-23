cf52d961f7e25cd8751e9d785b8e0d1d5e4a6cad
svetlana-zaletskay <svetka-pipetka10@mail.ru>
2017-05-23 20:49:30 +0300

    men-block-slick

--- js/fashion.js
+++ js/fashion.js
@@ -14,3 +14,34 @@ $(document).ready(function(){
         slidesToShow: 1
     });
 });
+
+$('#slickMenBlock').slick({
+    slidesToShow: 6,
+    responsive: [
+        {
+            breakpoint: 1400,
+            settings: {
+                arrows: false,
+                slidesToShow: 6
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '56px',
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '65px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
